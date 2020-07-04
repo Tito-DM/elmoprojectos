2cdc481cecf081454388e74010d52e107989cdd3
pyong <muandatito@gmail.com>
2020-07-04 13:01:17 +0100

    changed text in seu projecto page

--- src/components/SeuProjecto/SeuProjecto.js
+++ src/components/SeuProjecto/SeuProjecto.js
@@ -28,11 +28,11 @@ const SeuProjecto = () => {
 
 							<div className="input-field col s6">
 								<input id="first_name" type="text" className="validate" />
-								<label htmlFor="first_name">First Name</label>
+								<label htmlFor="first_name">Nome</label>
 							</div>
 							<div className="input-field col s6">
 								<input id="last_name" type="text" className="validate" />
-								<label htmlFor="last_name">Last Name</label>
+								<label htmlFor="last_name">Apelido</label>
 							</div>
 
 							<div className="input-field col s12">
